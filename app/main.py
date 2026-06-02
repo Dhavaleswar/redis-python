7841b121d957e0c81cd31a2c2ce3ca54e6bda34f
Dhavaleswar <93dhavaleshwar@gmail.com>
2026-06-02 15:02:57 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,30 @@ import socket  # noqa: F401
 import threading
 
 
+def parse_redis_type_protocol(decoded_data:str):
+    if decoded_data[0] == '*':
+        pass
+    elif decoded_data:
+        pass
+
+
+
+def create_output_msg_redis_type_protoc(message:str):
+    message_len = len(message)
+    return f"+{message_len}\r\n{message}\r\n".encode()
+
+
+
 def handle_client(connection:socket.socket) -> None:
     while True:
         data = connection.recv(1024)
-        # print("Recieved:", data.decode())
-        connection.sendall(b"+PONG\r\n")
+        decoded_data = data.decode()
+        if "*1\r\n$4\r\nPING\r\n" in decoded_data:
+            connection.sendall(b"+PONG\r\n")
+        elif decoded_data[0] == "*":
+            message_ = decoded_data.split("\r\n")[-2]
+            connection.send(f"${len(message_)}\r\n{message_}\r\n".encode())
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
